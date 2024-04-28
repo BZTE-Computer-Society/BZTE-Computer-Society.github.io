27a444acd6defd7a65c58438dc258bc1e058e2ec
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2024-04-28 21:24:23 +0430

    first commit

---
+++ data.js
@@ -0,0 +1,59 @@
+const data = {
+  aboutus: {
+    smalltxt: "درباره انجمن کامپیوتر",
+    title: " ایجاد تغیییر مبتنی بر همکاری و کار تیمی",
+    description:
+      " انجمن کامپیوتر نه تنها یک تیم از بچه های پرانگیزه بلکه یه جمع دوستانه هست که امید دارند بتوانند یک تغییر در زمینه علمی و فرهنگی دانشگاه انجام بدهند",
+    firstSrc: "images/teamworking.png",
+    secondSrc:
+      "https://cdn.tailgrids.com/2.0/image/marketing/images/about/about-01/image-2.jpg",
+    thirdSrc:
+      "https://cdn.tailgrids.com/2.0/image/marketing/images/about/about-01/image-1.jpg",
+  },
+  members: [
+    {
+      name: "محمد مهدی نباتی زاده",
+      field: "Graphic Designer | President",
+      resumeLink: "#",
+      imagesrc: "images/black-bmw.jpg",
+    },
+    {
+      name: "امیر محمد یزدان پناه",
+      field: "Back-end Developer | member",
+      resumeLink: "#",
+      imagesrc: "images/black-bmw.jpg",
+    },
+    {
+      name: "علیرضا شعبانی گملی",
+      field: "Front-end Developer | member",
+      resumeLink: "#",
+      imagesrc: "images/black-bmw.jpg",
+    },
+    {
+      name: "احسان درزی",
+      field: "PLC Developer | member",
+      resumeLink: "#",
+      imagesrc: "images/laptop-jpg.jpg",
+    },
+    {
+      name: "محمد مهدی الماسی خانلو",
+      field: "FullStack Developer | Honory member",
+      resumeLink: "#",
+      imagesrc: "images/black-bmw.jpg",
+    },
+    {
+      name: "محمد جواد مرادی",
+      field: "Deep Learning | Honory member",
+      resumeLink: "#",
+      imagesrc: "images/black-bmw.jpg",
+    },
+  ],
+  robotic: {
+    title: "اخبار | کلاس ربایتک",
+    subtitle: "برگزاری کلاس ربایتک ",
+    imagesrc: "images/black-bmw.jpg",
+    description:
+      "انجمن مهندسی کامپیوتر با کمک اساتید و دانشگاه کلاس های آمادگی برای شرکت در مسابقات ربایتک نرم افزاری را برای افراد مستعد در حوزه یادگیری عمیق و یادگیری ماشین و زبان برنامه نویسی پایتون برگزار می کند در نتیجه از افراد توانا در این حوزه ها دعوت به عمل  می آید تا بتوان با کمک همدیگر گامی جدید و افتخاری نو برای کشورمان بیافرینیم جهت اطلاع بیشتر لطفا فرم نظر سنجی را پر کنید",
+    link: "https://survey.porsline.ir/s/q8FR3awy",
+  },
+};
